2673be465984c9d89ddba6a6f6bb92b2cd497917
samtisi <samtisi@gmail.com>
2018-04-07 09:26:38 -0400

    before checking out to master

--- cards/Deck.py
+++ cards/Deck.py
@@ -2,7 +2,7 @@ from random import *
 from . import card
 class FiftyTwo:
 
-    def generate_deck(self):
+    def generate_deck():
         suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
         values = range(1, 14)
 
@@ -11,16 +11,16 @@ class FiftyTwo:
             for j in values:
                 placeholder_deck.append([i, j])
 
-        deck = []
+        deck_dealt = []
         for i in range(0, 52):
             new_card = card.Card()
-            deck.append(new_card)
+            deck_dealt.append(new_card)
 
         for i in range(0, 51):
             card_pick = randint(0, len(placeholder_deck) - 1)
             pick_a_card = placeholder_deck[card_pick]
             placeholder_deck.pop(card_pick)
-            deck[i].suit = pick_a_card[0]
-            deck[i].value = pick_a_card[1]
+            deck_dealt[i].suit = pick_a_card[0]
+            deck_dealt[i].value = pick_a_card[1]
 
-        return deck
+        return deck_dealt
