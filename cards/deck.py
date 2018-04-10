fcf2b8371e22299de247ebd2ef61e86d9d0bf2db
samtisi <samtisi@users.noreply.github.com>
2018-04-10 09:50:58 -0400

    cleaning up file names part 2 of 2

---
+++ cards/deck.py
@@ -0,0 +1,28 @@
+from random import *
+from cards import card
+class FiftyTwo:
+
+    def generate_deck():
+        suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+        values = range(1, 14)
+
+        placeholder_deck = []
+        for i in suits:
+            for j in values:
+                placeholder_deck.append([i, j])
+
+        deck_dealt = []
+        for i in range(0, 52):
+            new_card = card.Card()
+            deck_dealt.append(new_card)
+
+        for i in range(0, 51):
+            card_pick = randint(0, len(placeholder_deck) - 1)
+            pick_a_card = placeholder_deck[card_pick]
+            placeholder_deck.pop(card_pick)
+            deck_dealt[i].suit = pick_a_card[0]
+            deck_dealt[i].value = pick_a_card[1]
+
+        return deck_dealt
+
+
