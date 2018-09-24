0bcfdd90c60bd362b076b09a48612f33138f8483
samtisi <samtisi@users.noreply.github.com>
2018-09-24 13:55:10 -0400

    Dealing with the split
    Starting adding End of Hand conditions if split occurs.
    
    When this is complete I will need to make test scenarios to make sure it works properly.

--- actions/game.py
+++ actions/game.py
@@ -6,22 +6,34 @@ def split_hand(player_hand, shuffled_deck):
     hand_one.append(dealt_card)
     dealt_card, shuffled_deck = act_funcs.deal_card(shuffled_deck)
     hand_two.append(dealt_card)
+    scenario_1 = False
+    scenario_2 = False
+    hand_three = []
+    hand_four = []
     if hand_one[0] == hand_one[1]:
         split_1 = input("An opportunity to split again.  Do you take it?(Y/N)")
         if split_1 == "Y":
             hand_one, hand_three = hand_one[0], hand_one[1]
+            dealt_card, shuffled_deck = act_funcs.deal_card(shuffled_deck)
+            hand_one.append(dealt_card)
+            dealt_card, shuffled_deck = act_funcs.deal_card(shuffled_deck)
+            hand_three.append(dealt_card)
             scenario_1 = True
         else:
             scenario_1 = False
-            hand_three = 0
+            hand_three = []
     if hand_two[0] == hand_two[1]:
         split_2 = input("An opportunity to split again.  Do you take it?(Y/N)")
         if split_2 == "Y":
             hand_two, hand_four = hand_two[0], hand_two[1]
+            dealt_card, shuffled_deck = act_funcs.deal_card(shuffled_deck)
+            hand_two.append(dealt_card)
+            dealt_card, shuffled_deck = act_funcs.deal_card(shuffled_deck)
+            hand_four.append(dealt_card)
             scenario_2 = True
         else:
             scenario_2 = False
-            hand_four = 0
+            hand_four = []
     if scenario_1 == False and scenario_2 == False:
         return hand_one, hand_two, hand_three, hand_four, shuffled_deck, 1, 2
     if scenario_1 == True and scenario_2 == False:
@@ -69,6 +81,10 @@ def start_of_hand(player_hand, dealer_hand, shuffled_deck, bet, p):
     busted_two = False
     busted_three = False
     busted_four = False
+    hand_one = []
+    hand_two = []
+    hand_three = []
+    hand_four = []
     if act_funcs.dealer_win_check(dealer_hand) is True:
         print("Dealer has Blackjack! Everybody loses.")
         print("Their cards were:", dealer_hand[0], "and", dealer_hand[1])
@@ -88,7 +104,7 @@ def start_of_hand(player_hand, dealer_hand, shuffled_deck, bet, p):
         else:
             shuffled_deck, busted, player_hand = act_funcs.run_player_turn(player_hand, dealer_hand, shuffled_deck)
         #shuffled_deck, busted, player_hand = act_funcs.run_player_turn(player_hand, dealer_hand, shuffled_deck, p)
-    return shuffled_deck, busted, split, scenario_num, busted_one, busted_two, busted_three, busted_four
+    return shuffled_deck, busted, split, scenario_num, busted_one, busted_two, busted_three, busted_four, hand_one, hand_two, hand_three, hand_four
 
 
 def end_of_hand(busted, player_hand, dealer_hand, shuffled_deck, bet):
@@ -120,30 +136,55 @@ def end_of_hand(busted, player_hand, dealer_hand, shuffled_deck, bet):
     return result
 
 
-def end_of_hand_split(busted, player_hand, dealer_hand, shuffled_deck, bet, scenario_num):
+def end_of_hand_split(busted, player_hand, dealer_hand, shuffled_deck, bet, scenario_num, busted_one, busted_two, busted_three, busted_four, hand_one, hand_two, hand_three, hand_four):
+    result = 0
     if busted is False:
         dealer_hand, shuffled_deck = act_funcs.dealers_turn(dealer_hand, shuffled_deck)
-        if act_funcs.score_hand(dealer_hand) > 21:
-            print("Dealer busts! Everybody wins")
-            result = 0
-        elif act_funcs.score_hand(player_hand) > act_funcs.score_hand(dealer_hand):
-            print("Player wins!")
-            result = 1
-        elif act_funcs.score_hand(dealer_hand) == act_funcs.score_hand(player_hand):
-            print("Push")
-            result = 1
-        else:
-            print("Player loses.")
-            result = 0
-        print("Final score:")
-        hand_print = ''
-        for i in range(0,len(dealer_hand)):
-            hand_print += str(dealer_hand[i].value) + ' of ' + dealer_hand[i].suit + ', '
-        print("Dealer Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(dealer_hand))
-        hand_print = ''
-        for i in range(0, len(player_hand)):
-            hand_print += str(player_hand[i].value) + ' of ' + player_hand[i].suit + ', '
-        print("Player Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(player_hand))
+        if scenario_num == 1:
+            print("Hand #1")
+            if act_funcs.score_hand(dealer_hand) > 21:
+                print("Dealer busts! Everybody wins")
+                result = 0
+            elif act_funcs.score_hand(hand_one) > act_funcs.score_hand(dealer_hand):
+                print("Player's Hand #1 wins!")
+                result = result + 1
+            elif act_funcs.score_hand(dealer_hand) == act_funcs.score_hand(hand_one):
+                print("Hand #1 Push")
+                result = result + 1
+            else:
+                print("Player loses.")
+                result = 0
+            print("Final score:")
+            hand_print = ''
+            for i in range(0,len(dealer_hand)):
+                hand_print += str(dealer_hand[i].value) + ' of ' + dealer_hand[i].suit + ', '
+            print("Dealer Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(dealer_hand))
+            hand_print = ''
+            for i in range(0, len(hand_one)):
+                hand_print += str(hand_one[i].value) + ' of ' + hand_one[i].suit + ', '
+            print("Player Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(hand_one))
+            print("Hand #2")
+            if act_funcs.score_hand(dealer_hand) > 21:
+                print("Dealer busts! Everybody wins")
+                result = 0
+            elif act_funcs.score_hand(hand_two) > act_funcs.score_hand(dealer_hand):
+                print("Player's Hand #2 wins!")
+                result = result + 1
+            elif act_funcs.score_hand(dealer_hand) == act_funcs.score_hand(hand_two):
+                print("Hand #2 Push")
+                result = result + 1
+            else:
+                print("Player loses.")
+                result = 0
+            print("Final score:")
+            hand_print = ''
+            for i in range(0,len(dealer_hand)):
+                hand_print += str(dealer_hand[i].value) + ' of ' + dealer_hand[i].suit + ', '
+            print("Dealer Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(dealer_hand))
+            hand_print = ''
+            for i in range(0, len(hand_two)):
+                hand_print += str(hand_two[i].value) + ' of ' + hand_two[i].suit + ', '
+            print("Player Hand:", hand_print[:-2], '- Final Score: ', act_funcs.score_hand(hand_two))
     else:
         result = 0
     return result
@@ -162,12 +203,12 @@ def play_game(shuffled_deck, wallet, p):
         bet = 12
         scenario_num = 0
         dealer_hand, player_hand, shuffled_deck = act_funcs.first_deal(shuffled_deck)
-        shuffled_deck, busted, split, scenario_num = start_of_hand(player_hand, dealer_hand, shuffled_deck, bet, p)
+        shuffled_deck, busted, split, scenario_num, busted_one, busted_two, busted_three, busted_four, hand_one, hand_two, hand_three, hand_four = start_of_hand(player_hand, dealer_hand, shuffled_deck, bet, p)
 
         if scenario_num == 0:
             result = end_of_hand(busted, player_hand, dealer_hand, shuffled_deck, bet)
         else:
-            result = end_of_hand_split(busted, player_hand, dealer_hand, shuffled_deck, bet, scenario_num)
+            result = end_of_hand_split(busted, player_hand, dealer_hand, shuffled_deck, bet, scenario_num, busted_one, busted_two, busted_three, busted_four, hand_one, hand_two, hand_three, hand_four)
         wins = wins + result
         total = total + 1
         print(total)
